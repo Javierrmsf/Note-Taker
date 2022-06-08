8a02eb789450a90682a5444f93794335a58962d5
Javier R <javierresines@gmail.com>
2022-06-07 20:35:05 -0500

    adding get method to bring the index page and the notes html pages, also adding get method to bring stored notes

--- server.js
+++ server.js
@@ -5,7 +5,41 @@ const path = require("path");
 const fs = require("fs");
 
 
+//starting up express
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+////////
+
+
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.json());
+
+app.use(express.static("public"))
+
+////////
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+   });
+
+   ///route to index page 
+
+app.get("/notes", function(req, res) {
+ res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+   ///route to notes
+
+app.get("/api/notes", function(req, res){
+ res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+   //route to get stored notes from db
+
+
+
+
+   
+
